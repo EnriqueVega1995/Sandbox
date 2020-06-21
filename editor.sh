f209a463c301bb0184cfd4e7b0833c5f447048cc
Alex Rozgo <alex.rozgo@gmail.com>
2020-06-21 14:50:01 -0600

    support SimboticTorch environment variables

--- editor.sh
+++ editor.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-"$SIMBOTIC_ENGINE"/Engine/Binaries/Linux/UE4Editor "$SIMBOTIC_SANDBOX"/Sandbox.uproject $@
+export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
+export GST_PLUGIN_PATH=${SIMBOTIC_TORCH}/target/release:${LIBTORCH}/lib
+export RUST_BACKTRACE=1
 
+"$SIMBOTIC_ENGINE"/Engine/Binaries/Linux/UE4Editor "$SIMBOTIC_SANDBOX"/Sandbox.uproject $@
